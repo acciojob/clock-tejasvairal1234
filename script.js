54ebd9bc44566dc4166e4b431d94d25bd931a40b
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-01-31 22:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+setInterval(() => {
+  let now = new Date();
+
+  let dateTime = now.toLocaleString("en-US", {
+    month: "numeric",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true
+  });
+
+  document.getElementById("timer").innerHTML = dateTime;
+}, 1000);
